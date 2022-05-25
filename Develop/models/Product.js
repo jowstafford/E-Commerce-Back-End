af52926ef91c571d10e0a78aec71cf8a1a7218eb
jowstafford <91708768+jowstafford@users.noreply.github.com>
2022-05-25 14:54:11 -0500

    defined columns in Product.js. added comments based on instructions

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -10,6 +10,54 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    // Instructions in modules/Databse Models will be helpful for this
+    id: {
+      // Integer
+      type: DataTypes.INTEGER,
+      // Does not allow null values
+      allowNull: false,
+      // Set as primary key
+      primaryKey: true,
+      // Uses auto increment
+      autoIncrement: true
+    },
+    product_name: {
+      // String
+      type: DataTypes.STRING,
+      // Does not allow null values
+      allowNull: false
+    },
+    price: {
+      // Decimal
+      type: DataTypes.DECIMAL,
+      // Does not allow null values
+      allowNull: false,
+      // Validates that the value is a decimal
+      validate: {
+        isDecimal: true
+      }
+    },
+    stock: {
+      // Integer
+      type: DataTypes.INTEGER,
+      // Does not allow null values
+      allowNull: false,
+      // Set a default alue of 10
+      defaultValue: 10,
+      // Validates that the value is numeric
+      Validate: {
+        isNumeric: true
+      }
+    },
+    category_id: {
+      // Integer
+      type: DataTypes.INTEGER,
+      // References the category model's id
+      references: {
+        model: 'category',
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
