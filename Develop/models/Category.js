d43f3c78432e39027ca45f946627d4af98e1261c
jowstafford <91708768+jowstafford@users.noreply.github.com>
2022-05-25 14:43:47 -0500

    defined columns in Category.js. added comments based on instructions

--- Develop/models/Category.js
+++ Develop/models/Category.js
@@ -6,7 +6,24 @@ class Category extends Model {}
 
 Category.init(
   {
-    // define columns
+    // define columns 
+    // Instructions in modules/Databse Models will be helpful for this
+    id: {
+      // Integer
+      type: DataTypes.INTEGER,
+      // Does not allow null values
+      allowNull: false,
+      // Set as primary key
+      primaryKey: true,
+      // Uses auto increment
+      autoIncrement: true
+    },
+    category_name: {
+      // String
+      type: DataTypes.STRING,
+      // Does not allow null values
+      allowNull: false
+    }
   },
   {
     sequelize,
